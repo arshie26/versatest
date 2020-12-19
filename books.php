39d700ddd4d438ad35d9e8ce020a9af880552d88
Arsh Agarwal <arshie26@gmail.com>
2020-12-19 15:51:16 -0500

    Updated books to use asynchronous fetch requests and made the page more responsive

--- books.php
+++ books.php
@@ -10,66 +10,109 @@
 	    		<a href="versatest.php">Arsh Agarwal</a>
 	    	</h1>
 	    	<ul>
-	    		<li>Coding</li>
+	    		<li><a href="coding.php">Coding</a></li>
 	    		<li><a href="photography.php">Photography</a></li>
 	    		<li><a href="tutoring.php">Tutoring</a></li>
 	            <li><a href="books.php">Book List</a></li>
 	    	</ul>	
 	    </center>	
 	</navbar>
-	<div style="display:flex;">
-		<div id="demo" style = "min-width:40vw; max-width: 50vw; padding-left: 10vw;">
+	<div class="book-wrapper">
+		<div class="fiction" >
 				Fiction
+			<div id="9780590353403"></div>
+			<div id="9780439064873"></div>
+			<div id="9780439136365"></div>
+			<div id="9780439139601"></div>
+			<div id="9780439358071"></div>
+			<div id="9780439785969"></div>
+			<div id="9780545139700"></div>
 		</div>
-		<div id="nonf" style = "min-width:30vw; max-width: 50vw;padding-left: 10vw;">
+		<div class="nonf" >
 				Non Fiction
+			<div id="9780743269513"></div>
+			<div id="0071401946"></div>
+			<div id="9780449203651"></div>
+			<div id="9781416549710"></div>
+			<div id="9781591842804"></div>
 		</div>
 	</div>
 	<script>
             var bookList = ["9780590353403","9780439064873","9780439136365"," 9780439139601","9780439358071","9780439785969","9780545139700"]
+            var promiseList = [];
             for(var i = 0; i < bookList.length; i++){
-            	var request = new XMLHttpRequest();
-	            var jsonData;
-	            var proxy = 'https://cors-anywhere.herokuapp.com/';
-	            var bookCoverElem = document.createElement("img");
-	            var titleElem = document.createElement("p");
+            	var proxy = 'https://cors-anywhere.herokuapp.com/';
 	            var url = proxy + "http://openlibrary.org/api/books?bibkeys=ISBN:"+bookList[i]+"&format=json&jscmd=data";
+            	/*var request = new XMLHttpRequest();
+	            var jsonData;
+	            var bookCoverElem = document.createElement("img");
+	            bookCoverElem.className = "cover";
+	            var titleElem = document.createElement("p");
+	            
 	            request.onreadystatechange = function(){
 	            	if(this.readyState == 4 && this.status == 200){
 	            		jsonData = JSON.parse(this.responseText);
-			            bookData = jsonData["ISBN:" + bookList[i]];
+	            		var isbn = Object.keys(jsonData)[0];
+			            bookData = jsonData[isbn];
 			            titleElem.innerHTML = bookData.title;
 			            bookCoverElem.setAttribute("src", bookData.cover.medium);
-			            document.getElementById("demo").appendChild(titleElem);
-			            document.getElementById("demo").appendChild(bookCoverElem);
+			            document.getElementsByClassName("fiction")[0].appendChild(titleElem);
+			            document.getElementsByClassName("fiction")[0].appendChild(bookCoverElem);
 	            	}
 	            }
+				
 	            request.open("GET", url, false);
 	            //request.setRequestHeader("Access-Control-Allow-Origin", "*")
-	            request.send();	
+	            request.send();
+	            */
+	            promiseList.push(fetch(url)
+	            	.then((response) => response.json())
+	            	.then(function(data){ 
+	            		var isbn = Object.keys(data)[0];
+	            		var bookCoverElem = document.createElement("img");
+			            bookCoverElem.className = "cover";
+			            var titleElem = document.createElement("p");
+			            titleElem.innerHTML = data[isbn].title;
+			            bookCoverElem.setAttribute("src", data[isbn].cover.medium);
+	            		document.getElementById(isbn.substring(5)).appendChild(titleElem);
+	            		document.getElementById(isbn.substring(5)).appendChild(bookCoverElem);
+	            	}));	
             }
 
             var nfbookList = ["9780743269513","0071401946","9780449203651","9781416549710","9781591842804"]
 			for(var i = 0; i < nfbookList.length; i++){
-            	var request = new XMLHttpRequest();
+            	var proxy = 'https://cors-anywhere.herokuapp.com/';
+            	var url = proxy + "http://openlibrary.org/api/books?bibkeys=ISBN:"+nfbookList[i]+"&format=json&jscmd=data";
+            	/*var request = new XMLHttpRequest();
 	            var jsonData;
-	            var proxy = 'https://cors-anywhere.herokuapp.com/';
 	            var bookCoverElem = document.createElement("img");
+	            bookCoverElem.className = "cover";
 	            var titleElem = document.createElement("p");
-	            var url = proxy + "http://openlibrary.org/api/books?bibkeys=ISBN:"+nfbookList[i]+"&format=json&jscmd=data";
 	            request.onreadystatechange = function(){
 	            	if(this.readyState == 4 && this.status == 200){
 	            		jsonData = JSON.parse(this.responseText);
 			            bookData = jsonData["ISBN:" + nfbookList[i]];
 			            titleElem.innerHTML = bookData.title;
 			            bookCoverElem.setAttribute("src", bookData.cover.medium);
-			            document.getElementById("nonf").appendChild(titleElem);
-			            document.getElementById("nonf").appendChild(bookCoverElem);
+			            document.getElementsByClassName("nonf")[0].appendChild(titleElem);
+			            document.getElementsByClassName("nonf")[0].appendChild(bookCoverElem);
 	            	}
 	            }
 	            request.open("GET", url, false);
 	            //request.setRequestHeader("Access-Control-Allow-Origin", "*")
-	            request.send();	
+	            request.send();*/
+	            promiseList.push(fetch(url)
+	            	.then((response) => response.json())
+	            	.then(function(data){ 
+	            		var isbn = Object.keys(data)[0];
+	            		var bookCoverElem = document.createElement("img");
+			            bookCoverElem.className = "cover";
+			            var titleElem = document.createElement("p");
+			            titleElem.innerHTML = data[isbn].title;
+			            bookCoverElem.setAttribute("src", data[isbn].cover.medium);
+	            		document.getElementById(isbn.substring(5)).appendChild(titleElem);
+	            		document.getElementById(isbn.substring(5)).appendChild(bookCoverElem);
+	            	}));	
             }            
 /*            fetch("https://openlibrary.org/isbn/9780590353403")
             	.then(function(response){
