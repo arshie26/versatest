5ba5075a9de719f5b82a60ff187e195c0f1d5777
Arsh Agarwal <arshie26@gmail.com>
2020-12-28 17:03:29 -0500

    Added descriptions of my coding experience to the Coding page and modified style to make the text match theme and responsive

--- coding.php
+++ coding.php
@@ -10,21 +10,33 @@
     		<center>
 		    	<ul>
 		    		<li>
-		    			<a href="versatest.php"><h1>ARSH AGARWAL</h1></a>
+		    			<a class="title" href="versatest.php"><h1>ARSH AGARWAL</h1></a>
 		    		</li>
 		    	</ul>
 		    	<ul>
-		    		<li><a class="navbar" href="coding.php"><h3>CODING</h3></a></li>
-		    		<li><a class="navbar" href="photography.php"><h3>PHOTOGRAPHY</h3></a></li>
-		    		<li><a class="navbar" href="tutoring.php"><h3>TUTORING</h3></a></li>
-	                <li><a class="navbar" href="books.php"><h3>BOOK LIST</h3></a></li>
+		    		<li><a class="navbar" href="coding.php"><h3 class="navbar">CODING</h3></a></li>
+		    		<li><a class="navbar" href="photography.php"><h3 class="navbar">PHOTOGRAPHY</h3></a></li>
+		    		<li><a class="navbar" href="tutoring.php"><h3 class="navbar">TUTORING</h3></a></li>
+	                <li><a class="navbar" href="books.php"><h3 class="navbar">BOOK LIST</h3></a></li>
 		    	</ul>	
 		    </center>
 		</div>
 		<div id="cart-items"></div>
 		<div>
+			<p id="coding-desc">
+				Intergalactic Education is an e learning startup that has created a suite of video games to facilitate the development of STEM skills for the 21st century and the space economy. The games have been designed to engage students and simplify student development for teachers and parents.
+			</p>
+			<p id="coding-desc">
+				I was responsible for developing the website. I was able to learn the basics of C Sharp, HTML, and CSS in about 8 weeks to provide the required functionality and layout. I worked with the tech lead, CEO, game developer, and graphic designer to integrate the website with game and backend, incorporate appropriate branding onto the site, and provide functionality for the users of the website.
+			</p>
+			<p id="coding-desc">
+				The video below demonstrates typical usage of the video games and their integration with the website.  
+			</p>
 		<video style="width:80%; padding:10vw; padding-bottom:5vw;" controls><source src="igewebdev.mp4" type="video/mp4"></video>	
 		</div>
+		<p id="coding-desc" style="margin-bottom: 20px;">
+			After the CEO gave me the design prototypes, I developed the screens below to deliver student score reports in various degrees of granularity to users of the website. I used HTML tables and a lot of styling to develop the screens and make them responsive. On mobile, each entry becomes a <a href="https://magenic.com/thinking/making-tables-responsive-for-mobile-devices">card</a>.
+		</p>
 		<div>
 			<img id="scorereport" src="studentreport.png">
 		</div>
@@ -32,8 +44,14 @@
 			<img id = "verticalreport" src="gradingmobile.png">
 			<img id = "gradereport" src="grading2.png">
 		</div>
+		<p id = "coding-desc">
+			At the University of Houston, I worked with dancers and engineers to translate dance into an engineering language to facilitate experiments to discover how dance and expression are encoded in the brain. I particularly enjoyed the opportunity to translate different fields of academia as I collaborated with dancers and engineers. As part of my work, I created a data visualization script in Matlab to facilitate the translation.
+		</p>
+		<p id="coding-desc">
+			The script takes the translation work that I have done, maps it to appropriate dance expression qualities, and synchronizes dancer video with the expression. Several visual aids, like a map of the overall analysis on top and a color scheme for expression, help to simplify the viewing experience.
+		</p>
 		<video style="width:80%; padding:10vw;" controls><source src="datavis.mp4" type="video/mp4"></video>
-		<div id="cart-wrap" style="margin-bottom:20vw; text-align: center">
+		<div id="cart-wrap">
 	      <div id="coding-products"></div>
 	    </div>
 	</body>
