fd8c3183e05204585b98434150d000b3e0dac717
Arsh Agarwal <arshie26@gmail.com>
2020-12-29 14:51:23 -0500

    utoring page class

--- tutoring.php
+++ tutoring.php
@@ -24,19 +24,19 @@
 		<div id="cart-items"></div>
 		<div style="padding-bottom: 5vw;">
 			<video style = "padding-bottom: 5vw;" controls><source src="Let's Tutor_1.mp4" type="video/mp4"></video>
-			<p id="coding-desc">
+			<p class="coding-desc">
 					I know what it's like to struggle, but I love helping others learn. Learning is a process, and making sense of things is definitely easier with help. This is why I prioritize the student. I earn their trust so I can reach them.
 			</p>	
-			<p id="coding-desc">
+			<p class="coding-desc">
 				I'm patient with students, so they feel safe. They've seen the problem solved in class, but when they try to solve it themselves, they get stuck. My patience helps them make the necessary connections for themselves. They thoroughly understand the subject, theoretically and practically. My goal is to help them see the bigger picture, because seeing the bigger picture tends to make them independent.
 			</p>
-			<p id="coding-desc"> 
+			<p class="coding-desc"> 
 				As students go virtual, it's important for my voice to guide them. I use a steady, warm, enthusiastic tone that keeps the student moving with clarity. It catches them when they fail, nurtures them through frustration, and shares excitement when they succeed.  Students know they'll have a safe and exciting journey with me.
 			</p> 
-			<p id="coding-desc">
+			<p class="coding-desc">
 				I work intensely with the student the same way I worked with engineers and dancers to translate dance into an engineering language, earned customer service awards in IT, and created a website at Intergalactic Education. I understand the student’s needs quickly to help them achieve the required understanding.
 			</p>
-			<p id="coding-desc">
+			<p class="coding-desc">
 				Some of my favorite opportunities at college had to do with translating unique perspectives. For instance, at the University of Houston, I crafted a data visualization tool that helped artists translate their movements into an engineering language. It was a wonderful chance to help students with different backgrounds collaborate.
 				So no matter what your study needs are, I’m here to help!
 			</p>
